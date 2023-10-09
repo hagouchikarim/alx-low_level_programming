cde7230f0cbf70c99ad3922678e2e7fd2cec009c
hagouchikarim <hagouchik@gmail.com>
2023-10-09 08:31:41 -0700

    Print Numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
