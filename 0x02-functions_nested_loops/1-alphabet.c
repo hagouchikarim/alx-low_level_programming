da30a8787a2fad36e086f173224da33f8d1967bb
hagouchikarim <hagouchik@gmail.com>
2023-10-10 14:07:45 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+}
+
